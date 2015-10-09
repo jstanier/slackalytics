996135a31124ad853116fd3cef8b91845cbb03f9
James Stanier <jamess@brandwatch.com>
2015-10-09 13:33:58 +0100

    Format dates in human-readable way

--- api/src/main/java/com/jstanier/slackalytics/api/domain/AuthorCounts.java
+++ api/src/main/java/com/jstanier/slackalytics/api/domain/AuthorCounts.java
@@ -1,11 +1,15 @@
 package com.jstanier.slackalytics.api.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.jstanier.slackalytics.api.constants.Constants;
+
 import java.util.Date;
 
 public class AuthorCounts {
 
     private String channel;
     private String author;
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern= Constants.DATE_FORMAT, timezone="UTC")
     private Date date;
     private Long messages;
 
--- api/src/main/java/com/jstanier/slackalytics/api/domain/ChannelCounts.java
+++ api/src/main/java/com/jstanier/slackalytics/api/domain/ChannelCounts.java
@@ -1,10 +1,14 @@
 package com.jstanier.slackalytics.api.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.jstanier.slackalytics.api.constants.Constants;
+
 import java.util.Date;
 
 public class ChannelCounts {
 
     private String channel;
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern= Constants.DATE_FORMAT, timezone="UTC")
     private Date date;
     private Long messages;
 
